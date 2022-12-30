2188120ebd40c063d47fa4132cfc8f8f8e8bfe2e
Alex Devlin <acdevlin42@gmail.com>
2022-12-30 13:58:15 -0800

    Prompt user to enable location for offline functionality
    Also slightly change XML formatting and some text prompts

--- starter/app/src/main/java/com/google/codelabs/maps/placesdemo/LatLongActivity.kt
+++ starter/app/src/main/java/com/google/codelabs/maps/placesdemo/LatLongActivity.kt
@@ -1,20 +1,29 @@
 package com.google.codelabs.maps.placesdemo
 // Via https://medium.com/@hasperong/get-current-location-with-latitude-and-longtitude-using-kotlin-2ef6c94c7b76
+
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.GoogleApi
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.openlocationcode.OpenLocationCode
+import android.provider.Settings
+import android.system.Os.accept
+import kotlinx.coroutines.NonCancellable.cancel
+
 
 class LatLongActivity : AppCompatActivity() /*, LocationListener*/ {
     private lateinit var locationManager: LocationManager
@@ -32,9 +41,9 @@ class LatLongActivity : AppCompatActivity() /*, LocationListener*/ {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         setContentView(R.layout.activity_latlong)
-        title = "Location App"
-        val button: Button = findViewById(R.id.getLocation)
+        title = "Offline Plus Codes"
 
+        val button: Button = findViewById(R.id.getLocation)
         button.setOnClickListener {
             getLocation()
         }
@@ -48,17 +57,39 @@ class LatLongActivity : AppCompatActivity() /*, LocationListener*/ {
         // locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location : Location? ->
-                // Got last known location. In some rare situations this can be null.
-                onLocationChanged(location)
+                if(location == null) {
+                    if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                        promptEnableLocation()
+                    }
+                    else {
+                        Toast.makeText(this, "Could not get location.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                else {
+                    // Got last known location successfully
+                    onLocationChanged(location)
+                }
             }
     }
 
-    private fun onLocationChanged(location: Location?) {
-        // TODO handle null location gracefully
+    private fun promptEnableLocation() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle("GPS is Turned Off!")
+            .setMessage("Please enable location services to continue.")
+            .setPositiveButton("YES") { dialog, whichButton ->
+                startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            }
+            .setNegativeButton("NO") { dialog, whichButton ->
+                Toast.makeText(this, "Could not get location.", Toast.LENGTH_SHORT).show()
+            }
+            .show()
+    }
+
+    private fun onLocationChanged(location: Location) {
         tvGpsLocation = findViewById(R.id.latLongTextView)
-        val plusCode = OpenLocationCode.encode(location!!.latitude, location!!.longitude)
-        val preciseCode = OpenLocationCode(location!!.latitude, location!!.longitude, CODE_PRECISION_EXTRA)
-        tvGpsLocation.text = "Latitude: " + location!!.latitude + " , Longitude: " + location!!.longitude + " , PlusCode: " + plusCode + " , PreciseCode: " + preciseCode
+        val plusCode = OpenLocationCode.encode(location.latitude, location.longitude)
+        val preciseCode = OpenLocationCode(location.latitude, location.longitude, CODE_PRECISION_EXTRA)
+        tvGpsLocation.text = "Latitude: " + location.latitude + " , Longitude: " + location.longitude + " , PlusCode: " + plusCode + " , PreciseCode: " + preciseCode
     }
 
     /*
